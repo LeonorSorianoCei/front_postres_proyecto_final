6b6606053b4f52d0371d4f66d04bf6561ef9b40b
LeonorSorianoCei <leonor.sorianoexposito@alumnos.cei.es>
2024-04-21 15:51:31 +0200

    entorno

--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -188,7 +188,7 @@ function UsersList() {
                 {activeUsers.map(({ _id, nombre, imagen, descripcion }) => (
                   <div className="card-user" key={_id}>
                     <h3>{nombre}</h3>
-                    <img src={`${VITE_BACKEND_URL}/files/${imagen}`} alt={nombre} />
+                    <img src={`${VITE_BACKEND_URL}API/v1/files/${imagen}`} alt={nombre} />
                     <section className='texto'>
                       <p>{descripcion}</p>
                     </section>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -38,7 +38,7 @@ function Home() {
               }
   
               { isLogged  &&
-                <img className='imagen-usuario' src={`${VITE_BACKEND_URL}/files/${datosUsuario.imagen}`} alt={datosUsuario.nombre} />
+                <img className='imagen-usuario' src={`${VITE_BACKEND_URL}API/v1/files/${datosUsuario.imagen}`} alt={datosUsuario.nombre} />
               }
             </div>
           </section>
